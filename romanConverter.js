f5466c1847d1501e6549b0fb82c14d4d2f95f264
Juan Santos Poyato <jsantospoyato@gmail.com>
2023-01-15 20:57:39 +0100

    added examples + clean coding

--- romanConverter.js
+++ romanConverter.js
@@ -15,19 +15,20 @@ const ROMAN = {
     1: "I"
 }
 
+/**
+ * Converts decimal to Roman
+ */
 function convertToRoman(num) {
     // We conver the number to array of digits to be able to fetch them
     let vals = num.toString().split("");
     let str = "";
 
     // For each digit, we associate the correct value
-    var len = vals.length;
-    for (var i = 0; i < len; i++) {
+    const len = vals.length;
+    for (let i = 0; i < len; i++)
         if (vals[i] != 0)
             str += getRoman(
-                parseInt(getDigit(vals[i], vals.length - i - 1))
-            );
-    }
+                parseInt(getDigit(vals[i], vals.length - i - 1)));
 
     return str;
 }
@@ -60,12 +61,12 @@ function calculate(d) {
         .filter(val => val < d);
 
     // We calculate the next char based on the values of the digits
-    var cont = 0;
-    var index = values.length - 1;
+    let cont = 0;
+    let index = values.length - 1;
 
     // Always the value doesn't surpass the digit and there's more subvalues
     while (cont != d && index >= 0) {
-        var val = parseInt(values[index]);
+        const val = parseInt(values[index]);
 
         // If the value were to be bigger than the actual number, we get lower values
         if (cont + val > d) {
@@ -79,3 +80,9 @@ function calculate(d) {
 
     return str;
 }
+
+// Examples with different numbers
+console.log(convertToRoman(3));
+console.log(convertToRoman(900));
+console.log(convertToRoman(100));
+console.log(convertToRoman(88));
