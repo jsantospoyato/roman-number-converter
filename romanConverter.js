6b72f19bcfceb96f15cba97ad2e0400e7a47b84c
Juan Santos Poyato <jsantospoyato@gmail.com>
2023-01-15 20:42:57 +0100

    initial commit

---
+++ romanConverter.js
@@ -0,0 +1,81 @@
+// We declare the roman numbers
+const ROMAN = {
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I"
+}
+
+function convertToRoman(num) {
+    // We conver the number to array of digits to be able to fetch them
+    let vals = num.toString().split("");
+    let str = "";
+
+    // For each digit, we associate the correct value
+    var len = vals.length;
+    for (var i = 0; i < len; i++) {
+        if (vals[i] != 0)
+            str += getRoman(
+                parseInt(getDigit(vals[i], vals.length - i - 1))
+            );
+    }
+
+    return str;
+}
+
+/**
+ * Returns the equivalent number without lower number units
+ */
+function getDigit(d, index) {
+    return d + '000'.slice(3 - index);
+}
+
+/**
+ * Returns the correct ROMAN value of a given digit
+ */
+function getRoman(d) {
+    return ROMAN.hasOwnProperty(d)
+        ? ROMAN[d]
+        : calculate(d);
+}
+
+/**
+ * Calculates the roman string for a non listed Roman digit
+ */
+function calculate(d) {
+    let str = "";
+
+    // We get the minor values that the number so we can add up
+    const values = Object
+        .keys(ROMAN)
+        .filter(val => val < d);
+
+    // We calculate the next char based on the values of the digits
+    var cont = 0;
+    var index = values.length - 1;
+
+    // Always the value doesn't surpass the digit and there's more subvalues
+    while (cont != d && index >= 0) {
+        var val = parseInt(values[index]);
+
+        // If the value were to be bigger than the actual number, we get lower values
+        if (cont + val > d) {
+            index--;
+        } else {
+            // If it can fit, we add the count and we add the char to the string
+            cont += val;
+            str += ROMAN[val];
+        }
+    }
+
+    return str;
+}
